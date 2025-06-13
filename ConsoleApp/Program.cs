de95ec62532c55f5039524d6f41e09f10529b210
Administrator <Administrator@DESKTOP-4UNEB7C>
2025-06-13 17:50:33 +0900

    프로그래밍 기초 시작

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World! 김홍규 강사 입니다");
+            Console.WriteLine("Add:" + Add(10, 20));
+            ValMain();
+            CritcalAttackMain();
+            StageMain();
+            AttackWile();
+            MonsterListMain();
+        }
+        
+        static int Add(int a, int b)//10,20
+        {
+            int c = a + b; // 10 + 20 = 30
+            return c; //30
+        }
+
+        static void ValMain()
+        {
+            int nScore = 0;
+            float fRat = 1.0f / 4.0f;
+            Console.WriteLine("Score:" + nScore);
+            Console.WriteLine("Rat:" + fRat);
+        }
+
+        static void CritcalAttackMain()
+        {
+            Console.WriteLine("CritcalAttackMain");
+        }
+
+        static void StageMain()
+        {
+            Console.WriteLine("StageMain");
+        }
+
+        static void AttackWile()
+        {
+            Console.WriteLine("AttackWile");
+        }
+
+        static void MonsterListMain()
+        {
+            Console.WriteLine("MonsterListMain");
+        }
+    }
+}
