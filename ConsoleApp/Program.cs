ebdc1fc6b7ff9f10d4d77c56d09e54dfa5044cf5
Administrator <Administrator@DESKTOP-4UNEB7C>
2025-06-19 17:28:11 +0900

    절차지향을 이용한 전투함수 구현하기

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -13,7 +13,8 @@
             //AttackWhile();
             //AttackCritcalWhile();
             //MonsterListMain();
-            PlayerBattleMain(); // This call is now valid
+            //PlayerBattleMain();
+            MonsterSelectMain();
         }
 
         static int Add(int a, int b)//10,20
@@ -38,7 +39,7 @@
         {
             int nMonsterAtk = 10;
             int nPlayerHP = 100;
-            Console.WriteLine("몬스터의 공격력: " + nMonsterAtk + " 남은 hp:" + nPlayerHP);
+            Console.WriteLine("몬스터의 공격력: "+nMonsterAtk+" 남은 hp:" + nPlayerHP);
             nPlayerHP = nPlayerHP - nMonsterAtk;
             Console.WriteLine("몬스터의 공격력: " + nMonsterAtk + " 남은 hp:" + nPlayerHP);
         }
@@ -60,24 +61,24 @@
             int nRandom = cRandom.Next(1, 3);// 1~2의 값이 나온다. 1/2
             //int nRandom = cRandom.Next(0, 3);// 0.1,2의 값이 나온다. 1/3
             /*
-             if(nRandom == 1)
-             {
-                 Console.WriteLine("Critcal Attakc!");
-                 nMonsterHP = nMonsterHP - (int)(nPlayerAtk *1.5f);//몬스터를 때린다.
-             }
-             else
-                 nMonsterHP = nMonsterHP - nPlayerAtk;//몬스터를 때린다.
-             */
+            if(nRandom == 1)
+            {
+                Console.WriteLine("Critcal Attakc!");
+                nMonsterHP = nMonsterHP - (int)(nPlayerAtk *1.5f);//몬스터를 때린다.
+            }
+            else
+                nMonsterHP = nMonsterHP - nPlayerAtk;//몬스터를 때린다.
+            */
             if (nRandom == 1)
             {
                 Console.WriteLine("Critcal Attakc!");
                 nPlayerAtk = (int)(nPlayerAtk * 1.5f);
             }
-
-            nMonsterHP = nMonsterHP - nPlayerAtk;//몬스터를 때린다.
+            
+             nMonsterHP = nMonsterHP - nPlayerAtk;//몬스터를 때린다.
             //5
             Console.WriteLine("몬스터의 공격력: " + nPlayerAtk + " 남은 hp:" + nMonsterHP);
-            Console.WriteLine("Random:" + nRandom);
+            Console.WriteLine("Random:"+nRandom);
         }
         //마을,필드,상점 중에서 이동장소를 입력하면 그 장소의 이름이 나오는 프로그램작성.
         //데이터: 마을,필드,상점, 입력값
@@ -90,7 +91,7 @@
             Console.WriteLine("이동 할 장소를 입력하세요.(마을,사냥터,상점)");
             string strInput = Console.ReadLine();
 
-            switch (strInput)
+            switch(strInput)
             {
                 case "마을":
                     Console.WriteLine("마을 입니다.");
@@ -106,25 +107,25 @@
                     break;
             }
             /*
-             if (strInput == strTown)
-             {
-                 Console.WriteLine("마을 입니다.");
-             }
-             else if (strInput == strField)
-             {
-                 Console.WriteLine("상점 입니다.");
-             }
-             else if (strInput == strStore)
-             {
-                 Console.WriteLine("사냥터 입니다.");
-             }
-             else
-             {
-                 Console.WriteLine("장소를 잘못입력했습니다.");
-             }
+            if (strInput == strTown)
+            {
+                Console.WriteLine("마을 입니다.");
+            }
+            else if (strInput == strField)
+            {
+                Console.WriteLine("상점 입니다.");
+            }
+            else if (strInput == strStore)
+            {
+                Console.WriteLine("사냥터 입니다.");
+            }
+            else
+            {
+                Console.WriteLine("장소를 잘못입력했습니다.");
+            }
 
-             Console.WriteLine("StageMain");
-             */
+            Console.WriteLine("StageMain");
+            */
         }
         //몬스터가 플레이어를 (죽을때까지: 플레이어의 hp가 0이 될때) 공격한다.
         static void AttackWhile()
@@ -133,7 +134,7 @@
             int nMonsterAtk = 11;
             int nPlayerHP = 100;
             //살아있을때 공격을한다. //코드가 쉽다 -> 코드가 짧다. //햇깔리지않는다 -> 이조건을 그대로 생각한다.
-            while (true)
+            while(true)
             {
                 Console.WriteLine("공격전, 몬스터의 공격력: " + nMonsterAtk + " 남은 hp:" + nPlayerHP);
                 if (nPlayerHP <= 0) break;
@@ -143,7 +144,7 @@
         }
         //몬스터가 플레이어를 죽을때까지 (일정확률로 크리티컬)이 발생 공격한다. 
         //몬스터가 플레이어를 죽을때까지 공격할때 공격하기전에 확률을 계산하여 크리티컬데미지를 추가하여 공격하고, 크리티컬이 터지지않으면, 데미지가 증가되지않는다.
-
+        
         //몬스터가 플레이어를 공격한다.        
         //A.먼저 반복복을 돌리는 경우
         //1.몬스터가 플레이어를 일단 계속 공격한다 
@@ -170,7 +171,7 @@
                 {
                     int nCritcalAttack = (int)(nMonsterAtk * 1.5); //크리티컬데미지를 미리저장해서 알기쉽게 계산해둔다.
                     nPlayerHP = nPlayerHP - nCritcalAttack; //공격을할때 1회성으로 계산된 데미지를 사용한다.
-                    Console.WriteLine("크리티컬 데미지: " + nCritcalAttack);
+                    Console.WriteLine("크리티컬 데미지: "+nCritcalAttack);
                 }
                 else
                     nPlayerHP = nPlayerHP - nMonsterAtk;
@@ -183,39 +184,41 @@
         //플레이어가 (공격:데미지)하면 몬스터는 (반격:맞고나서 공격(대상에게 데미지)을 하고, 둘중하나가 죽을때(플레이어나 몬스터의 hp가 0보다 작거나 같을때)까지 전투가 끝나지않고, 한쪽이 죽으면 끝남.
         //데이터: 플레이어의 공격력, 플레이어의 체력, 몬스터의 공격력, 몬스터의 체력
         //알고리즘: 플레이어가 먼저 공격하고, 몬스터가 맞고나서 공격 한다. 한쪽이 죽을때까지.
-
-        static void PlayerBattleMain() // Changed from 'void' to 'static void'
+        
+        static void PlayerBattleMain()
         {
             Console.WriteLine("PlayerBattleMain");
 
-            int nPlayerHP = 100; // Initialized for demonstration
-            int nPlayerAtk = 10; // Initialized for demonstration
+            int nPlayerHP = 20;
+            int nPlayerAtk = 10;
 
-            int nMosterHP = 50;  // Initialized for demonstration
-            int nMosterAtk = 5;  // Initialized for demonstration
+            int nMonsterHP = 30;
+            int nMonsterAtk = 10;
 
-            // Example battle logic (you'll need to expand this for a full battle)
-            while (nPlayerHP > 0 && nMosterHP > 0)
+            //while(true) //프로래밍 문법에서 이러한 사용은 금기시되므로 추후, 아래와 같이 브레이크문의 반대조건을 추가하거나 조건을 모두 넣어 만일에 사태를 대비한다.
+            while (nPlayerHP > 0 || nMonsterHP > 0)
             {
-                // Player attacks monster
-                nMosterHP = nMosterHP - nPlayerAtk;
-                Console.WriteLine($"플레이어가 몬스터를 공격! 몬스터 남은 HP: {nMosterHP}");
-
-                if (nMosterHP <= 0)
-                {
-                    Console.WriteLine("몬스터를 처치했습니다!");
-                    break;
-                }
-
-                // Monster attacks player
-                nPlayerHP = nPlayerHP - nMosterAtk;
-                Console.WriteLine($"몬스터가 플레이어를 공격! 플레이어 남은 HP: {nPlayerHP}");
-
+                //플레이어는 몬스터를 언제 공격하지않는가? //플레이어가 언제 공격을 멈추는가? //이 모든 조건이 일반적인 인간에게는 당연한것이다.
                 if (nPlayerHP <= 0)
                 {
-                    Console.WriteLine("플레이어가 쓰러졌습니다!");
+                    Console.WriteLine("플레이어가 사망했다!");
                     break;
                 }
+                Console.WriteLine("플레이어가 몬스터를 공격한다.");
+                nMonsterHP = nMonsterHP - nPlayerAtk; //플레이어가 몬스터를 공격한다.
+                Console.WriteLine("Player Atk/HP:" + nPlayerAtk + "/" + nPlayerHP);
+                Console.WriteLine("Monster Atk/HP:" + nMonsterAtk + "/" + nMonsterHP);
+
+                //몬스터가 죽었을때는 몬스터가 공격할수없다.
+                if (nMonsterHP <= 0)
+                {
+                    Console.WriteLine("몬스터가 사망했다!");
+                    break;
+                }
+                Console.WriteLine("몬스터가 플레이어를 공격한다.");
+                nPlayerHP = nPlayerHP - nMonsterAtk;
+                Console.WriteLine("Player Atk/HP:" + nPlayerAtk + "/" + nPlayerHP);
+                Console.WriteLine("Monster Atk/HP:" + nMonsterAtk + "/" + nMonsterHP);
             }
         }
 
@@ -228,8 +231,8 @@
             listMonster.Add("Zombie");
             listMonster.Add("Dragon");
 
-            Console.WriteLine("[0]" + listMonster[0]);
-            Console.WriteLine("[3]" + listMonster[3]);
+            Console.WriteLine("[0]"+listMonster[0]);
+            Console.WriteLine("[3]"+listMonster[3]);
 
             for (int i = 0; i < listMonster.Count; i++)
             {
@@ -243,29 +246,29 @@
 
             string strInput = Console.ReadLine();
 
-            int nMonsterAttack = 10;
+            int nMonsterAtk = 10;
             int nMonsterHP = 100;
 
             switch (strInput)
             {
                 case "평원":
                     Console.WriteLine("슬라임이 출연합니다.");
-                    nMonsterAttack = 5;
+                    nMonsterAtk = 5;
                     nMonsterHP = 20;
                     break;
                 case "무덤":
                     Console.WriteLine("스켈레톤 출연합니다.");
-                    nMonsterAttack = 10;
+                    nMonsterAtk = 10;
                     nMonsterHP = 30;
                     break;
                 case "던전":
                     Console.WriteLine("좀비 출연 합니다.");
-                    nMonsterAttack = 20;
+                    nMonsterAtk = 20;
                     nMonsterHP = 50;
                     break;
                 case "계곡":
                     Console.WriteLine("드래곤이 출연 합니다.");
-                    nMonsterAttack = 50;
+                    nMonsterAtk = 50;
                     nMonsterHP = 200;
                     break;
                 default:
@@ -273,7 +276,35 @@
                     break;
             }
 
+            int nPlayerHP = 20;
+            int nPlayerAtk = 10;
+
             //여기에 전투코드를 삽입하면 작동한다.
+            while (nPlayerHP > 0 || nMonsterHP > 0)
+            {
+                //플레이어는 몬스터를 언제 공격하지않는가? //플레이어가 언제 공격을 멈추는가? //이 모든 조건이 일반적인 인간에게는 당연한것이다.
+                if (nPlayerHP <= 0)
+                {
+                    Console.WriteLine("플레이어가 사망했다!");
+                    break;
+                }
+                Console.WriteLine("플레이어가 몬스터를 공격한다.");
+                nMonsterHP = nMonsterHP - nPlayerAtk; //플레이어가 몬스터를 공격한다.
+                Console.WriteLine("Player Atk/HP:" + nPlayerAtk + "/" + nPlayerHP);
+                Console.WriteLine("Monster Atk/HP:" + nMonsterAtk + "/" + nMonsterHP);
+
+                //몬스터가 죽었을때는 몬스터가 공격할수없다.
+                if (nMonsterHP <= 0)
+                {
+                    Console.WriteLine("몬스터가 사망했다!");
+                    break;
+                }
+                Console.WriteLine("몬스터가 플레이어를 공격한다.");
+                nPlayerHP = nPlayerHP - nMonsterAtk;
+                Console.WriteLine("Player Atk/HP:" + nPlayerAtk + "/" + nPlayerHP);
+                Console.WriteLine("Monster Atk/HP:" + nMonsterAtk + "/" + nMonsterHP);
+            }
         }
     }
 }
+
